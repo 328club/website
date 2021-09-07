267409884dda94a87cfdb635495cb17c76459548
loonie <loonie@aliyun.com>
2021-09-07 10:28:23 +0800

    feat: spa root set lucky page

--- src/router/routers.ts
+++ src/router/routers.ts
@@ -1,11 +1,11 @@
 // interface
 import { RouteRecordRaw } from "vue-router";
 //pages
-import Home from "../pages/Home.vue";
+// import Home from "../pages/Home.vue";
 import Lucky from "../pages/Lucky.vue";
 
 const routes: RouteRecordRaw[] = [
-  { path: "/", component: Home },
+  { path: "/", component: Lucky },
   { path: "/lucky", component: Lucky },
 ];
 
